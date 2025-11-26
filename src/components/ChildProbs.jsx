7d63c6e7f695d76cd4108457acf634c925d57c84
Mohamed Kasim <kasim151000@gmail.com>
2025-11-26 23:26:19 +0530

    props task added1

--- src/components/ChildProbs.jsx
+++ src/components/ChildProbs.jsx
@@ -5,7 +5,7 @@ const ChildProbs = ({name,age,powers,biodatas,collegues}) => {
     <>
        <div className='w-full h-140 flex flex-col justify-center items-center gap-4' >
         
-            <div className='bg-orange-400 w-100 h-40'>
+            <div className='bg-orange-400 w-130 h-40'>
             <p className='text-2xl font-bold'>Movie Character:</p>
             <p className='text-xl'>Name: {name}</p>
             <p className='text-xl'>Age: {age}</p>
@@ -14,7 +14,7 @@ const ChildProbs = ({name,age,powers,biodatas,collegues}) => {
             </div>
             
            
-            <div className='w-100 h-40'>
+            <div className='w-130 h-40'>
               <p className='text-2xl font-bold'>Colleagues Details:</p>
              <p className='text-xl'>Name: {collegues[0].name}</p>
              <p className='text-xl'>Age: {collegues[0].Age}</p>
@@ -22,7 +22,7 @@ const ChildProbs = ({name,age,powers,biodatas,collegues}) => {
               <p className='text-xl'>Name: {collegues[1].name}</p>
              <p className='text-xl'>Age: {collegues[1].Age}</p>
             </div>
-             <div className='bg-green-800 w-100 h-40'>
+             <div className='bg-green-800 w-130 h-40'>
             <p className='text-2xl font-bold '>Bio deatils:</p>
             <p className='text-xl'>Name: {biodatas.name}</p>
             <p className='text-xl'>Age: {biodatas.name}</p>
